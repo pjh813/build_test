4376f064447b3103b08af47eb3f5ac5f071ef5fd
재현 <qkrwogus813@naver.com>
2024-02-24 23:56:25 +0900

    base file

--- foodapp/lib/main.dart
+++ foodapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:flutter_config/flutter_config.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -14,7 +15,6 @@ import 'package:fluttertoast/fluttertoast.dart';
 //MainScreen 코드
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-
   //gpt api key load
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Hive.initFlutter();
